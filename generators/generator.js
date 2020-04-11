0098ca2557d07666e053405ae4b64218ddf044f3
Robert Manzke <robert.manzke@fh-kiel.de>
2020-04-11 10:13:15 +0200

    generator updates

--- generators/generator.js
+++ generators/generator.js
@@ -22,22 +22,49 @@
 
 // this file is to parse the JSON mui files to generate the c++ code for parameter parsing
 // a convenience effort towards a generator
-// usage: node generator.js mui.jsn header.hpp source.cpp mp.jsn
-// if only mui.jsn is given output is on console, OTHERWISE THE hpp AND cpp SOURCE FILES ARE OVERWRITTEN!!!
-// header.hpp and source.cpp need special comment sections for code insertion like below
-// sectionHpp
-// sectionHpp
+// usage to create new files: node generator.js muifile
+    // example node generator.js mui-SimpleVCA.jsn
+// usage to overwrite / replace in existing files: node generator.js ProcessorName -i
+    // example node generator.js SimpleVCA
+    // attention this replaces code / overwrites the mp-...jsn file
 
 const fs = require('fs');
 let nargs = process.argv.length;
-let fn = process.argv[2];
 console.log("Have params: " + nargs);
 if(nargs < 3){
     console.log('Usage, see source file!');
     process.exit(-1);
 }
-let mui = JSON.parse(fs.readFileSync(fn));
+
+// process source files
+let muiIn, mui, hppIn, hppOut, cppIn, cppOut, mp;
+if(nargs == 3){
+    muiIn = process.argv[2];
+    mui = JSON.parse(fs.readFileSync(muiIn));
+    hppIn = 'ctagSoundProcessorTemplate.hpp';
+    hppOut = 'ctagSoundProcessor' + mui.id + '.hpp';
+    cppIn = 'ctagSoundProcessorTemplate.cpp';
+    cppOut = 'ctagSoundProcessor' + mui.id + '.cpp';
+    mp = 'mp-' + mui.id + '.jsn';
+}else if(nargs == 4){
+    if(process.argv[3] != '-i'){
+        console.log('Usage, see source file!');
+        process.exit(-1);
+    }
+    muiIn = '../spiffs_image/data/sp/mui-' + process.argv[2] + '.jsn';
+    mui = JSON.parse(fs.readFileSync(muiIn));
+    mp = '../spiffs_image/data/sp/mp-' + process.argv[2] + '.jsn';
+    hppIn = '../components/ctagSoundProcessor/ctagSoundProcessor' + process.argv[2] + '.hpp';
+    hppOut = hppIn;
+    cppIn = '../components/ctagSoundProcessor/ctagSoundProcessor' + process.argv[2] + '.cpp';
+    cppOut = cppIn;
+}else{
+    console.log('Usage, see source file!');
+    process.exit(-1);
+}
+// parse mui file
 let sHeader = 'const string id = "' + mui.id + '";\n';
+sHeader += 'void setIsStereo(){isStereo = ' + mui.isStereo + ';}\n';
 let sCpp1 = '';
 let sCpp2 = '';
 let smp = {}; // preset object to be written as json
@@ -45,48 +72,40 @@ smp.activePatch = 0;
 smp.patches = [];
 smp.patches.push({"name": "Default", "params": []});
 mui.params.forEach(el => parseData(el));
+// create mp preset model
+fs.writeFileSync(mp, JSON.stringify(smp, null, 2));
+// generate header file
+let shpp = fs.readFileSync(hppIn, 'utf8');
+shpp = shpp.replace(/ctagSoundProcessorTemplate/g, 'ctagSoundProcessor' + mui.id);
+let part = shpp.split('// sectionHpp');
+part.splice(1, 1); // remove auto code section, to be replaced with new generation
+let newHpp = part[0];
+newHpp += '// sectionHpp\n';
+newHpp += sHeader;
+newHpp += '// sectionHpp\n';
+newHpp += part[1];
+fs.writeFileSync(hppOut, newHpp);
+// generate source file
+// part 1
+let scpp = fs.readFileSync(cppIn, 'utf8');
+scpp = scpp.replace(/ctagSoundProcessorTemplate/g, 'ctagSoundProcessor' + mui.id);
+part = scpp.split('// sectionCpp0');
+part.splice(1, 1);
+let newCpp = part[0];
+newCpp += '// sectionCpp0\n';
+newCpp += sCpp1;
+newCpp += '// sectionCpp0\n';
+newCpp += part[1];
+// part 2
+part = newCpp.split('// sectionCpp1');
+part.splice(1, 1);
+newCpp = part[0];
+newCpp += '// sectionCpp1\n';
+newCpp += sCpp2;
+newCpp += '// sectionCpp1\n';
+newCpp += part[1];
+fs.writeFileSync(cppOut, newCpp);
 
-if(nargs == 3){
-    console.log(sHeader);
-    console.log(sCpp1);
-    console.log(sCpp2);
-    console.log(JSON.stringify(smp, null, 2));
-}else if(nargs == 6){
-    let hpp = process.argv[3];
-    let cpp = process.argv[4];
-    let mp = process.argv[5];
-    // create mp preset model
-    fs.writeFileSync(mp, JSON.stringify(smp, null, 2));
-    // generate header file
-    let shpp = fs.readFileSync(hpp, 'utf8');
-    let part = shpp.split('// sectionHpp');
-    part.splice(1, 1); // remove auto code section, to be replaced with new generation
-    let newHpp = part[0];
-    newHpp += '// sectionHpp\n';
-    newHpp += sHeader;
-    newHpp += '// sectionHpp\n';
-    newHpp += part[1];
-    fs.writeFileSync(hpp, newHpp);
-    // generate source file
-    // part 1
-    let scpp = fs.readFileSync(cpp, 'utf8');
-    part = scpp.split('// sectionCpp0');
-    part.splice(1, 1);
-    let newCpp = part[0];
-    newCpp += '// sectionCpp0\n';
-    newCpp += sCpp1;
-    newCpp += '// sectionCpp0\n';
-    newCpp += part[1];
-    // part 2
-    part = newCpp.split('// sectionCpp1');
-    part.splice(1, 1);
-    newCpp = part[0];
-    newCpp += '// sectionCpp1\n';
-    newCpp += sCpp2;
-    newCpp += '// sectionCpp1\n';
-    newCpp += part[1];
-    fs.writeFileSync(cpp, newCpp);
-}
 
 
 function parseData(el){
