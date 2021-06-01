ac1af18087df9069b0c1fe79b5dc885697bcbffb
Felix Wiegand <fxwiegand@gmail.com>
2021-06-01 17:17:44 +0200

    Update custom-build.yml Actually remove files

--- .github/remove-apps.py
+++ .github/remove-apps.py
@@ -1,6 +1,11 @@
 import sys
+import os
 
 apps = sys.stdin.read().split("#")
 print("The following apps will not be included in your custom firmware:")
 for app in apps:
-    print(app)
+    os.remove(f"spiffs_image/data/sp/mp-{app}.jsn")
+    os.remove(f"spiffs_image/data/sp/mui-{app}.jsn")
+    os.remove(f"components/ctagSoundProcessor/ctagSoundProcessor{app}.hpp")
+    os.remove(f"components/ctagSoundProcessor/ctagSoundProcessor{app}.cpp")
+
