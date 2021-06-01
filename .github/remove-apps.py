36d9a8e731ed5da597b3ee22c9c6d6696203ebc7
Felix Wiegand <fxwiegand@gmail.com>
2021-06-01 17:31:36 +0200

    Remove print

--- .github/remove-apps.py
+++ .github/remove-apps.py
@@ -3,7 +3,6 @@ import os
 
 apps = sys.stdin.read().split("#")
 apps = map(lambda x: x.strip(), apps)
-print("The following apps will not be included in your custom firmware:")
 for app in apps:
     os.remove(f"spiffs_image/data/sp/mp-{app}.jsn")
     os.remove(f"spiffs_image/data/sp/mui-{app}.jsn")
