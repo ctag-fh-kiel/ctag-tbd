7aae4427ba1a1868d7ffe9e3a67c412fbe051884
Felix Wiegand <fxwiegand@gmail.com>
2021-06-01 17:28:58 +0200

    Use strip()

--- .github/remove-apps.py
+++ .github/remove-apps.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 apps = sys.stdin.read().split("#")
-apps = map(lambda x: x.trim(), apps)
+apps = map(lambda x: x.strip(), apps)
 print("The following apps will not be included in your custom firmware:")
 for app in apps:
     os.remove(f"spiffs_image/data/sp/mp-{app}.jsn")
