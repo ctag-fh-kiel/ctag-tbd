78cbe6a576afdd9c3ab9c4144b97c1c4ec05ec7c
Felix Wiegand <fxwiegand@wgdnet.de>
2021-08-22 16:49:17 +0200

    Update flash script

--- .github/generate-flash-script.py
+++ .github/generate-flash-script.py
@@ -7,13 +7,25 @@ regex = re.compile("/opt/esp/python_env/idf4\.1_py3\.6_env/bin/python \.\./\.\./
 command = regex.findall(input)[0]
 command = command.replace("/opt/esp/python_env/idf4.1_py3.6_env/bin/python", "python")
 command = command.replace("../../opt/esp/idf/components/esptool_py/esptool/esptool.py", "$ESP_TOOL_PATH")
+command = command.replace("(PORT)", "$PORT")
 command = command.replace("build/", "")
 command = command.replace("bootloader/", "")
 command = command.replace("partition_table/", "")
 
+ite = """
+if [ $# -le 1 ]
+  then
+    echo "Usage: $0 ESP_TOOL_PATH PORT"
+    echo "Using default parameters $ESP_TOOL_PATH $PORT"
+  else
+    ESP_TOOL_PATH=$1
+    PORT=$2
+fi
+"""
+
 with open("flash.sh", "w") as flash_script:
     flash_script.write("#!/bin/sh\n")
-    flash_script.write("# Change ESP_TOOL_PATH to the location of your esptool.py\n")
-    flash_script.write("# Change PORT to the port your tbd is connected\n")
-    flash_script.write('export ESP_TOOL_PATH="/opt/esp/idf/components/esptool_py/esptool/esptool.py"\n')
+    flash_script.write('PORT="/dev/tty.SLAB_USBtoUART"\n')
+    flash_script.write('ESP_TOOL_PATH="/opt/esp/idf/components/esptool_py/esptool/esptool.py"\n')
+    flash_script.write(ite)
     flash_script.write(command)
