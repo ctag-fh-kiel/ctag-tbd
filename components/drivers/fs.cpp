978c6b5bac87c7d87e375296f9073e4bbabfafe5
Robert Manzke <robert.manzke@fh-kiel.de>
2023-10-15 21:42:11 +0200

    migrated to littlefs

--- components/drivers/fs.cpp
+++ components/drivers/fs.cpp
@@ -24,40 +24,43 @@ respective component folders / files if different from this license.
 #include <sys/stat.h>
 #include "esp_err.h"
 #include "esp_log.h"
-#include "esp_spiffs.h"
+#include "esp_littlefs.h"
 #include "fs.hpp"
 
 using namespace CTAG::DRIVERS;
 
 void FileSystem::InitFS() {
-    ESP_LOGI("fs", "Initializing SPIFFS");
+    ESP_LOGI("fs", "Initializing LITTLEFS");
 
-    esp_vfs_spiffs_conf_t conf = {
+    esp_vfs_littlefs_conf_t conf = {
             .base_path = "/spiffs",
             .partition_label = "storage",
-            .max_files = 5,
-            .format_if_mount_failed = false
+            .partition = NULL,
+            .format_if_mount_failed = true,
+            .read_only = false,
+            .dont_mount = false,
+            .grow_on_mount = 1
     };
 
     // Use settings defined above to initialize and mount SPIFFS filesystem.
     // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    esp_err_t ret = esp_vfs_littlefs_register(&conf);
 
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
             ESP_LOGE("fs", "Failed to mount or format filesystem");
         } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE("fs", "Failed to find SPIFFS partition");
+            ESP_LOGE("fs", "Failed to find LITTLEFS partition");
         } else {
-            ESP_LOGE("fs", "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+            ESP_LOGE("fs", "Failed to initialize LITTLEFS (%s)", esp_err_to_name(ret));
         }
         return;
     }
 
     size_t total = 0, used = 0;
-    ret = esp_spiffs_info("storage", &total, &used);
+    ret = esp_littlefs_info("storage", &total, &used);
     if (ret != ESP_OK) {
-        ESP_LOGE("fs", "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+        ESP_LOGE("fs", "Failed to get LITTLEFS partition information (%s)", esp_err_to_name(ret));
     } else {
         ESP_LOGI("fs", "Partition size: total: %d, used: %d", total, used);
     }
