d23cb1a90daea34308ec7ad4a9a1e3e12f443645
Robert Manzke <robert.manzke@fh-kiel.de>
2023-12-17 17:35:07 +0100

    MacOscDuo optimized

--- components/ctagSoundProcessor/ctagSoundProcessorMacOscDuo.hpp
+++ components/ctagSoundProcessor/ctagSoundProcessorMacOscDuo.hpp
@@ -48,9 +48,12 @@ namespace CTAG {
             braids::SignatureWaveshaper ws[2];
             braids::Quantizer quantizer[2];
             CTAG::SP::HELPERS::ctagADSREnv envelope[2];
+            CTAG::SP::HELPERS::ctagADSREnv envelopeHighRes[2];
             CTAG::SP::HELPERS::ctagSineSource lfo;
+            CTAG::SP::HELPERS::ctagSineSource lfoHighRes;
             const uint8_t sync1[32] = {0};
             const uint8_t sync2[32] = {0};
+            float smoothp0[2] {0.f, 0.f}, smoothp1[2] {0.f, 0.f};
             bool prevTrigger[2] = {false, false};
             const uint16_t bit_reduction_masks[7] = {
                     0xc000,
@@ -68,17 +71,14 @@ namespace CTAG {
 	atomic<int32_t> gain2, cv_gain2;
 	atomic<int32_t> pitch1, cv_pitch1;
 	atomic<int32_t> pitch2, cv_pitch2;
+	atomic<int32_t> tune1, cv_tune1;
+	atomic<int32_t> tune2, cv_tune2;
 	atomic<int32_t> decimation, cv_decimation;
 	atomic<int32_t> bit_reduction, cv_bit_reduction;
 	atomic<int32_t> q_scale, cv_q_scale;
 	atomic<int32_t> param_0, cv_param_0;
 	atomic<int32_t> param_1, cv_param_1;
 	atomic<int32_t> waveshaping, cv_waveshaping;
-	atomic<int32_t> fm_amt1, cv_fm_amt1;
-	atomic<int32_t> fm_amt2, cv_fm_amt2;
-	atomic<int32_t> am_amt, cv_am_amt;
-	atomic<int32_t> p0_amt, cv_p0_amt;
-	atomic<int32_t> p1_amt, cv_p1_amt;
 	atomic<int32_t> enableEG1, trig_enableEG1;
 	atomic<int32_t> enableEG2, trig_enableEG2;
 	atomic<int32_t> attack, cv_attack;
@@ -86,6 +86,11 @@ namespace CTAG {
 	atomic<int32_t> sustain, cv_sustain;
 	atomic<int32_t> release, cv_release;
 	atomic<int32_t> lfo_f, cv_lfo_f;
+	atomic<int32_t> fm_amt1, cv_fm_amt1;
+	atomic<int32_t> fm_amt2, cv_fm_amt2;
+	atomic<int32_t> am_amt, cv_am_amt;
+	atomic<int32_t> p0_amt, cv_p0_amt;
+	atomic<int32_t> p1_amt, cv_p1_amt;
 	// sectionHpp
         };
     }
