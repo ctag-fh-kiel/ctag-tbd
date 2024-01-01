a32c95e1a4baa32aaabc4e7be7f40e7f2d5d508e
Robert Manzke <robert.manzke@fh-kiel.de>
2024-01-01 13:56:21 +0100

    generator script updated

--- generators/ctagSoundProcessorTemplate.hpp
+++ generators/ctagSoundProcessorTemplate.hpp
@@ -7,7 +7,7 @@ namespace CTAG {
         public:
             virtual void Process(const ProcessData &) override;
             // no ctor, use Init() instead, is called from factory after successful creation
-            virtual void Init() override;
+            virtual void Init(std::size_t blockSize, void *blockPtr) override;
             virtual ~ctagSoundProcessorTemplate();
 
         private:
