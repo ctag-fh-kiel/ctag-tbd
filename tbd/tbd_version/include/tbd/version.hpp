aaa8409d8d2b0bede3081190d425728f2e7e2eb6
Michael Jonathan Lee <code.mjlee@gmail.com>
2024-11-12 11:43:34 +0000

    add TBD version info library

---
+++ tbd/tbd_version/include/tbd/version.hpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <cstdint>
+
+
+namespace CTAG {
+
+extern const std::string hardware_type;
+extern const std::string firmware_version;
+extern const std::string device_capabilities;
+
+extern const std::string commit;
+extern const bool post_commit_changes;
+extern const uint8_t ahead_of_last_release;
+extern const std::string build_date;
+
+}
