97a419e75ec81d45bec1692de64adb853682a6d4
Michael Jonathan Lee <code.mjlee@gmail.com>
2024-11-13 10:12:58 +0000

    add reconfiguring option to CLI

--- tools/tbd_tools/tbdtools/cli/firmware_cmd.py
+++ tools/tbd_tools/tbdtools/cli/firmware_cmd.py
@@ -3,6 +3,7 @@ import typer
 from subprocess import run
 
 from tbdtools.project import ProjectRoot
+from tbdtools.project.build_info import Platform
 
 
 firmware_group = typer.Typer()
@@ -14,14 +15,20 @@ def build_cmd(ctx: typer.Context):
     dirs: ProjectRoot = ctx.obj
     run(['idf.py', '-B', dirs.build.firmware(), 'build'], stdout=sys.stdout, stderr=sys.stderr)
 
+@firmware_group.command('reconfigure')
+def reconfigure_cmd(ctx: typer.Context):
+    """ build firmware """
+    dirs: ProjectRoot = ctx.obj
+    run(['idf.py', '-B', dirs.build.firmware(), 'reconfigure'], stdout=sys.stdout, stderr=sys.stderr)
+
 @firmware_group.command('clean')
-def build_cmd(ctx: typer.Context):
+def clean_cmd(ctx: typer.Context):
     """ remove build files """
     dirs: ProjectRoot = ctx.obj
     run(['idf.py', '-B', dirs.build.firmware(), 'clean'], stdout=sys.stdout, stderr=sys.stderr)
 
 @firmware_group.command('fullclean')
-def build_cmd(ctx: typer.Context):
+def fullclean_cmd(ctx: typer.Context):
     """ remove all build files and build config """
     dirs: ProjectRoot = ctx.obj
     run(['idf.py', '-B', dirs.build.firmware(), 'fullclean'], stdout=sys.stdout, stderr=sys.stderr)
