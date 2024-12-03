63d04c69acbad79b372302c0a9432ff5b84bc578
Michael Jonathan Lee <code.mjlee@gmail.com>
2024-12-03 06:42:29 +0000

    fix desktop build CV access segafault

--- ports/tbd_port_desktop/tbd_drivers_port/src/dynamic_cv.cpp
+++ ports/tbd_port_desktop/tbd_drivers_port/src/dynamic_cv.cpp
@@ -1,4 +1,11 @@
+#include <tbd/common/audio.hpp>
+
 #include <tbd/drivers/common/dynamic_cv.hpp>
+
+
+namespace {
+    uint8_t cv_data[TBD_CHUNK_BUFFER_SIZE + N_TRIGS] = {};
+}
 
 namespace tbd::drivers {
 
@@ -7,7 +14,7 @@ void DynamicCV::init() {
 }
 
 uint8_t* DynamicCV::update() {
-    return nullptr;
+    return cv_data;
 }
 
 void DynamicCV::flush() {
