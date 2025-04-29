7a13ca5c915e4821adab71c6dafaa5212389b019
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-04-29 22:50:31 +0200

    remove sound processor component since it is now part of tbd_audio

--- components/core/tbd_sound_processor/include/tbd/sound_processor/module.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace tbd::sound_processor {
-
-constexpr const char* tag = "sound_processor";
-
-}
--- components/core/tbd_sound_processor/preprocessor/__init__.py
+++
@@ -1,3 +0,0 @@
-from .process_plugins import *
-from .reflectables import *
-from .plugin_meta_generator import *
