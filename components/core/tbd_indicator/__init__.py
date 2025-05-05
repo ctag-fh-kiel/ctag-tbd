b41f382314bd45d52591e0e17f0892b6f63b7d7e
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-05-05 20:50:37 +0200

    fix indicator config esp only options

--- components/core/tbd_indicator/__init__.py
+++ components/core/tbd_indicator/__init__.py
@@ -1,10 +1,9 @@
 from esphome.core import CORE
 import esphome.config_validation as cv
-import esphome.codegen as cg
 from esphome import pins
-from pathlib import Path
 
-from esphome.components.tbd_module import new_tbd_component
+import esphome.components.tbd_module as tbd
+
 
 AUTO_LOAD = ['tbd_module']
 
@@ -15,30 +14,28 @@ CONF_GREEN_PN = 'green'
 CONF_BLUE_PIN = 'blue'
 
 
-if CORE.is_host:
-    CONFIG_SCHEMA = cv.Schema({})
-else:
-    CONFIG_SCHEMA = cv.Schema({
-        cv.Optional(CONF_PINS): {
-            cv.Required(CONF_RED_PIN): pins.gpio_output_pin_schema,
-            cv.Required(CONF_GREEN_PN): pins.gpio_output_pin_schema,
-            cv.Required(CONF_BLUE_PIN): pins.gpio_output_pin_schema,
-        },
-        cv.Optional(CONF_PIN): pins.gpio_output_pin_schema
-    })
+CONFIG_SCHEMA = cv.Schema({
+    cv.Optional(CONF_PINS): cv.All(cv.only_on_esp32, {
+        cv.Required(CONF_RED_PIN): pins.gpio_output_pin_schema,
+        cv.Required(CONF_GREEN_PN): pins.gpio_output_pin_schema,
+        cv.Required(CONF_BLUE_PIN): pins.gpio_output_pin_schema,
+    }),
+    cv.Optional(CONF_PIN): cv.All(cv.only_on_esp32, pins.gpio_output_pin_schema)
+})
 
-def add_rgb_pins(config):
+def add_rgb_pins(config, module: tbd.ComponentInfo):
     def get_pin_number(key):
         return f'GPIO_NUM_{config[key]['number']}'
 
-    cg.add_build_flag('-DTBD_INDICATOR_USE_RGB=1')
-    cg.add_build_flag(f'-DTBD_RGB_PIN_RED={get_pin_number(CONF_RED_PIN)}')
-    cg.add_build_flag(f'-DTBD_RGB_PIN_GREEN={get_pin_number(CONF_GREEN_PN)}')
-    cg.add_build_flag(f'-DTBD_RGB_PIN_BLUE={get_pin_number(CONF_BLUE_PIN)}')
+    module.add_define('TBD_INDICATOR_USE_RGB')
+    module.add_define(f'TBD_RGB_PIN_RED', get_pin_number(CONF_RED_PIN))
+    module.add_define(f'TBD_RGB_PIN_GREEN', get_pin_number(CONF_GREEN_PN))
+    module.add_define(f'TBD_RGB_PIN_BLUE', get_pin_number(CONF_BLUE_PIN))
 
 async def to_code(config):
-    new_tbd_component(__file__)
-    if not CORE.is_host:
+    module = tbd.new_tbd_component(__file__)
+
+    if not tbd.is_desktop():
         pin = config.get(CONF_PIN)
         pins = config.get(CONF_PINS)
 
@@ -49,10 +46,10 @@ async def to_code(config):
             raise ValueError('can not have both rgb pinout and neopixel pin')
 
         if pins is not None:
-            add_rgb_pins(config[CONF_PINS])
+            add_rgb_pins(config[CONF_PINS], module)
         else:
-            cg.add_build_flag('-DTBD_INDICATOR_USE_NEOPIXEL=1')
-            cg.add_build_flag(f'-DTBD_NEOPIXEL_PIN_DOUT=GPIO_NUM_{pin['number']}')
+            module.add_define('TBD_INDICATOR_USE_NEOPIXEL')
+            module.add_define('TBD_NEOPIXEL_PIN_DOUT', f'GPIO_NUM_{pin['number']}')
 
             from esphome.components.esp32 import add_idf_component
             add_idf_component(
@@ -61,7 +58,4 @@ async def to_code(config):
                 path="led_strip"
                 # refresh='1day'
             )
-
-            led_strip_include = Path(CORE.build_path) / 'components' / 'led_strip' / 'include'
-            cg.add_build_flag(f'-I{led_strip_include}')
 
