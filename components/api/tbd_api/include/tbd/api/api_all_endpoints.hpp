5a74158044ee9e28cfab09a878120d601b062b77
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-05-16 20:32:25 +0200

    split message list into requests and responses

--- components/api/tbd_api/include/tbd/api/api_all_endpoints.hpp
+++ components/api/tbd_api/include/tbd/api/api_all_endpoints.hpp
@@ -32,8 +32,11 @@ struct MessageInfo {
 extern const size_t NUM_ENDPOINTS;
 extern const Endpoint ENDPOINT_LIST[];
 
-extern const size_t NUM_MESSAGES;
-extern const MessageInfo MESSAGE_LIST[];
+extern const size_t NUM_REQUEST_MESSAGES;
+extern const MessageInfo REQUEST_MESSAGE_LIST[];
+
+extern const size_t NUM_RESPONSE_MESSAGES;
+extern const MessageInfo RESPONSE_MESSAGE_LIST[];
 
 template<typename Message> 
 bool decode_message(Message& in_message, const uint8_t* in_buffer, size_t len);
