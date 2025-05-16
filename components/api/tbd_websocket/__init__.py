877b7da30ca7a262bb9509203f7379ba24f4f2b3
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-05-16 20:42:57 +0200

    basic functional websocket server for host platform

--- components/api/tbd_websocket/__init__.py
+++ components/api/tbd_websocket/__init__.py
@@ -1,14 +1,45 @@
+from pathlib import Path
 import esphome.components.tbd_module as tbd
+from esphome.components.tbd_module.cmake_dependencies import cmake_dependency
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.const import CONF_PORT, CONF_PATH
 
-AUTO_LOAD = ['tbd_module']
 
-CONFIG_SCHEMA = cv.Schema({})
+AUTO_LOAD = ['tbd_api']
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.Optional(CONF_PORT, default='7777'): cv.port,
+    cv.Optional(CONF_PATH, '/ws'): str,
+})
 
 async def to_code(config):
-    tbd.new_tbd_component(__file__)
+    websocket_path = Path(config[CONF_PATH])
+    if not websocket_path.is_absolute:
+        websocket_path = Path('/') / websocket_path
+
+    component = tbd.new_tbd_component(__file__)
+    component.add_define('TBD_WEBSOCKET_PORT', config[CONF_PORT])
+    component.add_define('TBD_WEBSOCKET_PATH', f'"{websocket_path}"')
 
     websocket_server = cg.global_ns.namespace('tbd').namespace('api').namespace('WebsocketServer')
     cg.add(websocket_server.begin())
+
+    if tbd.is_desktop():
+        # cmake_dependency(
+        #     name='simplewebserver',
+        #     url='https://gitlab.com/eidheim/Simple-Web-Server.git',
+        #     ref='v3.1.1',
+        # )        
+        
+        cmake_dependency(
+            name='simplewebsocketserver',
+            url='https://gitlab.com/eidheim/Simple-WebSocket-Server.git',
+            ref='v2.0.2',
+        )
+        # cg.add_build_flag('-lssl')
+        cg.add_build_flag('-lcrypto')
+    elif tbd.is_esp32():
+        from esphome.components.esp32 import add_idf_sdkconfig_option
+        add_idf_sdkconfig_option('CONFIG_HTTPD_WS_SUPPORT', True)
---
+++ components/api/tbd_websocket/host/websocket_server.cpp
@@ -0,0 +1,60 @@
+#include <tbd/api/websocket_server.hpp>
+#include <tbd/websocket/module.hpp>
+
+#include <tbd/api.hpp>
+
+#include <tbd/logging.hpp>
+#include <server_ws.hpp>
+
+#include <memory>
+
+namespace {
+
+using tbd::websocket::tag;
+using WSServer = SimpleWeb::SocketServer<SimpleWeb::WS>;
+
+WSServer server;
+std::thread server_thread;
+std::promise<unsigned short> server_port;
+
+}
+
+namespace tbd::api {
+
+void WebsocketServer::begin() {
+    TBD_LOGI(tag, "starting websocket server");
+    
+    server.config.port = TBD_WEBSOCKET_PORT;
+    auto &echo = server.endpoint["^/ws/?$"];
+    echo.on_message = [](std::shared_ptr<WSServer::Connection> connection, std::shared_ptr<WSServer::InMessage> request) {
+        TBD_LOGI(tag, "got request");
+        std::string in_message = request->string();
+        size_t in_length = in_message.length();
+        size_t length = in_length > 128 ? in_length : 128;
+        uint8_t buffer[length];
+        for (size_t i = 0; i < in_message.length(); ++i) {
+          TBD_LOGI(tag, "%02x", in_message[i]);
+          buffer[i] = in_message[i];
+        }
+        if (Api::handle_stream_input(buffer, length) != errors::SUCCESS) {
+          return;
+        }
+        auto out_message = std::string_view(reinterpret_cast<char*>(buffer), length);
+        connection->send(out_message, nullptr, 130);
+      };
+    
+
+    server_thread = std::thread([]() {
+
+        server.start([](unsigned short port) {
+          server_port.set_value(TBD_WEBSOCKET_PORT);
+        });
+    });
+    
+}
+    
+void WebsocketServer::end() {
+
+}
+
+}
