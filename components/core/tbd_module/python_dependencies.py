7634c9d6b10067b8d30e98f785571daa458e42a2
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-05-24 11:41:47 +0200

    fix packages with differing imports always installing bug

--- components/core/tbd_module/python_dependencies.py
+++ components/core/tbd_module/python_dependencies.py
@@ -4,23 +4,24 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 
-def python_dependencies(*packages: list[str | tuple[str, str]]):
-    def ensure_package_is_installed(package: str):
-        if isinstance(package, tuple):
-            import_name, package_name = package
-        else:
-            import_name = package
-            package_name = package
+def ensure_package_is_installed(import_name: str, package_name: str):
+    import importlib
+    try:
+        importlib.import_module(import_name)
+    except ImportError:
+        import pip
+        pip.main(['install', package_name])
+        _LOGGER.warning(f'installed exterrnal python package {package_name}')
 
-        import importlib
-        try:    
-            importlib.import_module(import_name)
-        except ImportError:
-            import pip
-            pip.main(['install', package_name])
-            _LOGGER.warning(f'installed exterrnal python package {package_name}')
-            
+
+def python_dependencies(*packages: str | tuple[str, str]):
     for package in packages:
-        ensure_package_is_installed(package)
+        match package:
+            case (import_name, package_name):
+                ensure_package_is_installed(import_name, package_name)
+            case str():
+                ensure_package_is_installed(package, package)
+            case _:
+                raise ValueError(f'invalid python dependency description {package}')
 
 __all__ = ['python_dependencies']
