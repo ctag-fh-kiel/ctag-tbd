02852b605c1b0df9f4c19ae61b3a8587c75264cb
Robert Manzke <robert.manzke@fh-kiel.de>
2025-06-02 18:09:26 +0200

    RGB sound level indicator led working

--- main/Control.hpp
+++ main/Control.hpp
@@ -33,7 +33,7 @@ namespace CTAG{
             Control() = delete;
             static void Init();
             static void SetCVChannelBiPolar(bool const &v0, bool const &v1, bool const &v2, bool const &v3);
-            IRAM_ATTR static void Update(uint8_t **trigs, float **cvs);
+            IRAM_ATTR static void Update(uint8_t **trigs, float **cvs, uint32_t ledStatus);
         private:
 #ifndef CONFIG_TBD_PLATFORM_MK2
             DRAM_ATTR static uint8_t *trig_data;
