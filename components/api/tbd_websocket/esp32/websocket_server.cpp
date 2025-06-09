5a65cc3af719dcc48db5839140dc2b97be67b7d0
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-06-09 21:36:30 +0200

    add event sink for esp32 websocket server

--- components/api/tbd_websocket/esp32/websocket_server.cpp
+++ components/api/tbd_websocket/esp32/websocket_server.cpp
@@ -113,12 +113,11 @@ void end() {
 }
 
 [[tbd::sink]]
-void emit_event(const uint8_t* buffer, size_t length) {
+void emit_on_websocket(const uint8_t* buffer, size_t length) {
     httpd_ws_frame_t ws_pkt = {};
     ws_pkt.type = HTTPD_WS_TYPE_BINARY;
     ws_pkt.payload = const_cast<uint8_t*>(buffer);
     ws_pkt.len = length;
-    TBD_LOGI(tag, "Sending packet with message: %i", ws_pkt.len);
     if (const auto err = broadcast_send(&ws_pkt); err != ESP_OK) {
         ESP_LOGE(tag, "failed to send event");
     }
