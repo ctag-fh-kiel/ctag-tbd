8d82015c221a88b67188059dcf8d5d2763d89672
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-06-22 14:44:52 +0200

    add some test plugin classes for plugin meta testing

---
+++ components/core/tbd_test_plugins/plugin_src/test_plugin2.cpp
@@ -0,0 +1,15 @@
+#include <tbd/sounds/test_plugin2.hpp>
+
+namespace tbd::test_sounds {
+
+void Test2::init() {
+
+}
+
+void Test2::process_mono(const sound_processor::StereoSampleView& input,
+                        const sound_processor::MonoSampleView& output)
+{
+
+}
+
+}
---
+++ components/core/tbd_test_plugins/plugin_src/test_plugin3.cpp
@@ -0,0 +1,15 @@
+#include <tbd/sounds/test_plugin3.hpp>
+
+namespace tbd::test_sounds {
+
+void Test3::init() {
+
+}
+
+void Test3::process_mono(const sound_processor::StereoSampleView& input,
+                        const sound_processor::MonoSampleView& output)
+{
+
+}
+
+}
