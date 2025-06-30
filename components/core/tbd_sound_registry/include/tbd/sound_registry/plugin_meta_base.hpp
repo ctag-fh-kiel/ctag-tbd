0271ff7dcd4ec14dff5c48e11d1257a8c7e74e79
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-06-30 12:07:34 +0200

    use atomic flag in sound processor updates

--- components/core/tbd_sound_registry/include/tbd/sound_registry/plugin_meta_base.hpp
+++ components/core/tbd_sound_registry/include/tbd/sound_registry/plugin_meta_base.hpp
@@ -5,11 +5,12 @@
 #include <tbd/parameter_types.hpp>
 
 #include <atomic>
+#include <functional>
 
 namespace tbd::sound_registry {
 
 struct PluginMetaBase : sound_processor::SoundProcessor {
-    explicit PluginMetaBase(const parameters::PluginID id) : id_(id), operation_cleared_(true) {}
+    explicit PluginMetaBase(const parameters::PluginID id) : id_(id), operation_pending_(false) {}
     ~PluginMetaBase() override = default;
 
     virtual parameters::PluginID id() const { return id_; };
@@ -20,14 +21,13 @@ struct PluginMetaBase : sound_processor::SoundProcessor {
         if (const auto err = parameters::verify_int_parameter(id_, parameter_id); err != TBD_OK) {
             return err;
         }
-        if (!operation_cleared_) {
+        if (operation_pending_.test_and_set()) {
             return TBD_ERR(SOUND_REGISTRY_PENDING_OPERATION);
         }
 
         operation_.type = WriteOperation::SET_VALUE;
         operation_.parameter = parameter_id;
         operation_.payload.param = {.int_value = value};
-        operation_cleared_ = false;
         return TBD_OK;
     }
 
@@ -35,14 +35,13 @@ struct PluginMetaBase : sound_processor::SoundProcessor {
         if (const auto err = parameters::verify_uint_parameter(id_, parameter_id); err != TBD_OK) {
             return err;
         }
-        if (!operation_cleared_) {
+        if (operation_pending_.test_and_set()) {
             return TBD_ERR(SOUND_REGISTRY_PENDING_OPERATION);
         }
 
         operation_.type = WriteOperation::SET_VALUE;
         operation_.parameter = parameter_id;
         operation_.payload.param = {.uint_value = value};
-        operation_cleared_ = false;
         return TBD_OK;
     }
 
@@ -50,14 +49,13 @@ struct PluginMetaBase : sound_processor::SoundProcessor {
         if (const auto err = parameters::verify_float_parameter(id_, parameter_id); err != TBD_OK) {
             return err;
         }
-        if (!operation_cleared_) {
+        if (operation_pending_.test_and_set()) {
             return TBD_ERR(SOUND_REGISTRY_PENDING_OPERATION);
         }
 
         operation_.type = WriteOperation::SET_VALUE;
         operation_.parameter = parameter_id;
         operation_.payload.param = {.float_value = value};
-        operation_cleared_ = false;
         return TBD_OK;
     }
 
@@ -65,14 +63,13 @@ struct PluginMetaBase : sound_processor::SoundProcessor {
         if (const auto err = parameters::verify_trigger_parameter(id_, parameter_id); err != TBD_OK) {
             return err;
         }
-        if (!operation_cleared_) {
+        if (operation_pending_.test_and_set()) {
             return TBD_ERR(SOUND_REGISTRY_PENDING_OPERATION);
         }
 
         operation_.type = WriteOperation::SET_VALUE;
         operation_.parameter = parameter_id;
         operation_.payload.param = {.trigger_value = value};
-        operation_cleared_ = false;
         return TBD_OK;
     }
 
@@ -84,33 +81,33 @@ struct PluginMetaBase : sound_processor::SoundProcessor {
         {
             return err;
         }
-        if (!operation_cleared_) {
+        if (operation_pending_.test_and_set()) {
             return TBD_ERR(SOUND_REGISTRY_PENDING_OPERATION);
         }
 
         operation_.type = WriteOperation::SET_MAPPING;
         operation_.parameter = parameter_id;
         operation_.payload.mapping = input_id;
-        operation_cleared_ = false;
         return TBD_OK;
     }
 
 protected:
-    struct WriteOperation{
+    struct WriteOperation {
         enum {
             NOOP        = 0,
             SET_VALUE   = 1,
             SET_MAPPING = 2,
+            CALL        = 3,
         } type;
         parameters::ParameterID parameter;
         union {
             parameters::Mapping mapping;
             ParamValue param;
         } payload;
-    } operation_ =  {};
+    } operation_;
 
     parameters::PluginID id_;
-    std::atomic<bool> operation_cleared_;
+    std::atomic_flag operation_pending_;
 };
 
 }
