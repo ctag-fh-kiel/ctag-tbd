5bfb48aa9b9b3a769838055b4b83071ad34df685
Robert Manzke <robert.manzke@fh-kiel.de>
2025-07-04 18:50:58 +0200

    intermediate

--- components/ctagSoundProcessor/synthesis/DrumModel.hpp
+++ components/ctagSoundProcessor/synthesis/DrumModel.hpp
@@ -9,6 +9,5 @@ namespace CTAG::SYNTHESIS{
         virtual void Init() = 0;
         virtual void Trigger() = 0;
         virtual void Process(float* out, uint32_t size) = 0;
-        virtual void RenderControls() = 0;
     };
 }
