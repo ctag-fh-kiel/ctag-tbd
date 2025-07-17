a87e16c605910e5f868d44b60463d11b8f94ba20
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-07-17 21:14:43 +0200

    remove unused components and files

--- components/core/tbd_config/__init__.py
+++
@@ -1,5 +0,0 @@
-from esphome.components.tbd_module import new_tbd_component
-
-REQUIRES = ['tbd_module']
-
-new_tbd_component(__file__)
--- components/core/tbd_display/library/include/tbd/display.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <tbd/display/common/display.hpp>
