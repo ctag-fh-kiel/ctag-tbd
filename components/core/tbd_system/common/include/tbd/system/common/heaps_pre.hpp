5cfd9ad5770b8633c922f3400ce65d26528b827e
Michael Jonathan Lee <code.mjlee@gmail.com>
2025-07-23 10:33:29 +0200

    add DMA capability flag to capability flagged memory allocation

--- components/core/tbd_system/common/include/tbd/system/common/heaps_pre.hpp
+++ components/core/tbd_system/common/include/tbd/system/common/heaps_pre.hpp
@@ -7,6 +7,7 @@
 // heap types
 
 #define TBD_HEAPS_8BIT     (1 << 2)
+#define TBD_MALLOC_CAP_DMA (1 << 3)
 #define TBD_HEAPS_SPIRAM   (1 << 10)
 #define TBD_HEAPS_INTERNAL (1 << 11)
 #define TBD_HEAPS_DEFAULT  (1 << 12)
