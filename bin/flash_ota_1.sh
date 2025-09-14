9424e4564b4c0170dfbe1c8f31b740e4041b0835
Robert Manzke <robert.manzke@fh-kiel.de>
2025-09-14 09:34:46 +0200

    switch to OTA partition example

--- bin/flash_ota_1.sh
+++ bin/flash_ota_1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python $IDF_PATH/components/app_update/otatool.py --port /dev/cu.usbmodem101 write_ota_partition --name ota_1 --input tusb_msc.bin
+
+# switch to ota_1 partition
+#python $IDF_PATH/components/app_update/otatool.py --port /dev/cu.usbmodem101 switch_ota_partition --name ota_1
