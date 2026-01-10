602faaa7b718d1ac4951d437118cb4e2a74d4bfe
Robert Manzke <robert.manzke@fh-kiel.de>
2026-01-10 18:55:46 +0100

    back to zip, add zlib

--- create_sd_archive.sh
+++ create_sd_archive.sh
@@ -5,15 +5,15 @@ SOURCE_DIR="$1"
 BUILD_DIR="$2"
 XXH128SUM="$3"
 
-SD_CARD_TAR="${BUILD_DIR}/tbd-sd-card.tar"
+SD_CARD_ZIP="${BUILD_DIR}/tbd-sd-card.zip"
 SD_CARD_HASH="${BUILD_DIR}/tbd-sd-card-hash.txt"
 VERSION_FILE="${BUILD_DIR}/.version"
 
-echo "Creating SD card archive with deflate compression..."
-rm -f "${SD_CARD_TAR}"
+echo "Creating SD card archive..."
+rm -f "${SD_CARD_ZIP}"
 
 # Create temporary directory structure
-TEMP_DIR="${BUILD_DIR}/temp_archive_content"
+TEMP_DIR="${BUILD_DIR}/temp_zip_content"
 rm -rf "${TEMP_DIR}"
 mkdir -p "${TEMP_DIR}"
 
@@ -22,40 +22,27 @@ cp -r "${SOURCE_DIR}/spiffs_image/data" "${TEMP_DIR}/data"
 cp -r "${SOURCE_DIR}/spiffs_image/www" "${TEMP_DIR}/www"
 cp -r "${SOURCE_DIR}/sample_rom/tbdsamples" "${TEMP_DIR}/tbdsamples"
 
-# Create tar archive (uncompressed, just for structure)
+# Create zip from temp directory
 cd "${TEMP_DIR}"
-tar -cf "${SD_CARD_TAR}" \
+zip -r "${SD_CARD_ZIP}" \
     data \
     www \
-    tbdsamples
+    tbdsamples \
+    -x '*.DS_Store' '*/__pycache__/*'
 
 # Clean up temp directory
 cd "${BUILD_DIR}"
 rm -rf "${TEMP_DIR}"
 
-# Compress the tar with raw deflate (compatible with esp_rom tinfl)
-echo "Compressing with raw deflate..."
-gzip -c -n "${SD_CARD_TAR}" > "${SD_CARD_TAR}.gz"
-# Extract just the deflate stream (remove gzip header/trailer)
-python3 -c "
-import sys
-with open('${SD_CARD_TAR}.gz', 'rb') as f:
-    data = f.read()
-    # Skip gzip header (10 bytes min) and find deflate start
-    # gzip format: 10 byte header, deflate data, 8 byte trailer
-    deflate_data = data[10:-8]
-    with open('${SD_CARD_TAR}.deflate', 'wb') as out:
-        out.write(deflate_data)
-"
-rm -f "${SD_CARD_TAR}.gz"
-
 echo "Generating hash file..."
-"${XXH128SUM}" "${SD_CARD_TAR}.deflate" | awk '{print $1}' > "${SD_CARD_HASH}"
+"${XXH128SUM}" "${SD_CARD_ZIP}" | awk '{print $1}' > "${SD_CARD_HASH}"
 
-echo "Adding .version file..."
+echo "Adding .version file to archive..."
 cp "${SD_CARD_HASH}" "${VERSION_FILE}"
+cd "${BUILD_DIR}"
+zip -u "${SD_CARD_ZIP}" .version
+rm -f "${VERSION_FILE}"
 
-echo "SD card archive created: ${SD_CARD_TAR}.deflate"
-echo "Original tar: ${SD_CARD_TAR}"
+echo "SD card archive created: ${SD_CARD_ZIP}"
 echo "Hash file created: ${SD_CARD_HASH}"
 
